632d7866d4229970b42d916e29e9d71dde9543bb
daneg69 <85411714+daneg69@users.noreply.github.com>
2022-06-23 04:37:44 +0300

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Creates a class called Square which is empty
+"""
+
+
+class Square:
+    """ Empty class with private attribute
+    """
+    def __init__(self, size):
+        """
+                Instantiation with size
+        Args:
+            size: size of the square
+        """
+        self.__size = size
